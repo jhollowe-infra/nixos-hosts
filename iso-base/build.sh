d571c7c314de3e1b6c9a0f9942d6709967a2df5c
John Hollowell <johnahollowell@gmail.com>
2024-05-27 02:05:13 +0000

    Add iso-base and build script

---
+++ iso-base/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+use_flake=${1:-1}
+iso_link_name=nixos-custom-base.iso
+
+if [ $use_flake -eq 1 ]; then
+  nix build .#nixosConfigurations.iso-base.config.system.build.isoImage
+else
+  nix-build '<nixpkgs/nixos>' -A config.system.build.isoImage -I nixpkgs=channel:nixos-23.11 -I nixos-config=iso.nix
+fi
+
+if [ $? -eq 0 ]; then
+  # make a link to the built image
+  fileName=$(find result/iso -name '*.iso')
+  rm -f $iso_link_name ; ln -s $(realpath $fileName) $iso_link_name
+fi
---
+++ iso-base/configuration.nix
@@ -0,0 +1,50 @@
+{ lib, config, ... }:
+let
+  nixos-common = builtins.fetchGit {
+    url = "https://github.com/jhollowe-infra/nixos-common.git";
+    ref = "main";
+    rev = "a62bd558e56243e1bf051b4fee1b72891dd560da";
+  };
+in
+{
+  imports = [
+    "${nixos-common}/workloads/diag.nix"
+    "${nixos-common}/workloads/deep_diag.nix"
+  ];
+
+  # use zstd compression for the ISO image
+  isoImage.squashfsCompression = "zstd -Xcompression-level 19";
+
+  # enable "experimental" features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # enable SSH and allow pretty much any type of connection
+  services.openssh = {
+    settings = {
+      PermitRootLogin = "yes";
+      KbdInteractiveAuthentication = true;
+      PasswordAuthentication = true;
+    };
+    extraConfig = ''
+      PubkeyAuthentication yes
+      PermitEmptyPasswords yes
+      HostbasedAuthentication yes
+    '';
+  };
+
+  # make the root user super accessible
+  users.users = {
+    root = {
+      password = "password";
+      hashedPassword = lib.mkForce null;
+      hashedPasswordFile = lib.mkForce null;
+      initialHashedPassword = lib.mkForce null;
+      initialPassword = lib.mkForce null;
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILjyoZakOhGPmzJx3zH8vEizvfMbM5Aa8iTuP5VAk+QK 3:jhollowe@JOHN-DESKTOP.internal.johnhollowell.com"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDr9lnRhfAPce+yYxNMIL9EWa7dOl2u0vjq5qVM5P17i jhollowe@JOHN-LAPTOP.internal.johnhollowell.com"
+      ];
+    };
+
+  };
+}
